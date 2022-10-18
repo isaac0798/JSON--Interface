e9eb4dd331e691969e9ed2c7a3a71fcac6722fa1
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2022-10-18 19:06:06 +0100

    output file and scripts

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+import * as fs from 'fs';
 import basicJson from './JSONs/basic.json'
 
-console.log(basicJson);
+
+for (const [key, value] of Object.entries(basicJson)) {
+    console.log(`${key}: ${value}`);
+}
+
+fs.writeFile('./output.ts', "hello", (err) => {
+    if (err) {
+        return console.log(err)
+    }
+
+    console.log('dub')
+});

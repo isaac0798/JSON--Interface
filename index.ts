d27273b366b625b74ff56cfc69bb1a9856a88909
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2022-10-29 17:18:06 +0100

    initial first level object working

--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
 import * as fs from 'fs';
 import basicJson from './JSONs/basic.json'
 
+interface test {
+    data: string
+}
+
+let outputData: string = ''
+let outputObject = {};
 
 for (const [key, value] of Object.entries(basicJson)) {
     console.log(`${key}: ${value}`);
+    const valType = typeof value;
+    outputData = `${key}: ${valType}`;
 }
 
-fs.writeFile('./output.ts', "hello", (err) => {
+const indentString = (str: string, count: number, indent = ' ') =>
+  str.replace(/^/gm, indent.repeat(count));
+ 
+const removeString = (str: string): string => {
+    return str.replace(/['"]+/g, '')
+}
+
+const baseInterfaceString = 'interface root {' + '\n' + removeString(indentString(JSON.stringify(outputData),2)) + '\n' + '}';
+const buffer = Buffer.from(baseInterfaceString, 'utf-8');
+//const buffertest = Buffer.from(JSON.stringify(test));
+
+fs.writeFile('./output.ts', buffer, (err) => {
     if (err) {
         return console.log(err)
     }
---
+++ output.ts
@@ -0,0 +1,3 @@
+interface root {
+  name: string
+}
